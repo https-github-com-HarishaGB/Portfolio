cb63b123eb7df07b64fd68354294e50d09b7c214
HarishaGB <harshagb555@gmail.com>
2024-01-25 23:50:24 +0530

    portfolio

---
+++ src/Components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import img from "../images/gbimg.jpg";
+
+const About = () =>{
+    return(
+        <div className="about">
+            <h1>About Me</h1>
+            {/* <img src="https://images.unsplash.com/photo-1505968409348-bd000797c92e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/> */}
+            <img src={img} alt="wealth"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Labore quaerat aperiam ut veritatis voluptas, quae rem ipsam officia, 
+                voluptatibus cum commodi quibusdam accusantium nemo. Tempora ea id sunt quasi ut?
+            </p>
+        </div>
+    )
+}
+
+
+export default About
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return(
+        <nav className="navbar">
+            <h1>Harisha G B</h1>
+            <div className="menu">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contacts</a>
+                <a href="#">Skills</a>
+                <a href="#">Qualifications</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar
