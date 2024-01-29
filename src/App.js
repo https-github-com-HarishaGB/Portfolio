8cc55f33f544ea8a2a4aad72de5e0a364e269b8f
HarishaGB <harshagb555@gmail.com>
2024-01-29 21:16:17 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
+import Section from "./Components/section";
 
+import sectionData from "./data/sectionData";
 
 const App = () => {
+
+    let a = 100
+    let b = 50
 
     return(
         <div>
             <Navbar/>
             <About/>
+
+            {
+                sectionData.map((item, index) =>(
+                    <Section key={index}
+                    title={item.title}
+                    description={item.description}
+                    /> 
+                ))
+            }
+
+            {/* { a>b? (<h1>Greayer Number is {a}</h1>) : (<h1>Greater Number is {b}</h1>)} */}
+           
         </div>
     )
 }
 
 
 export default App
+
+// loops => higher order functions => map, filter, reduce
+// if else => ternery operator
+
+
+// {
+//     for(let t of sectionData){
+//         <section
+//         title={t.title}
+//         description={t.description}
+//         />
+//     }
+// }
---
+++ src/data/sectionData.js
@@ -0,0 +1,16 @@
+let sectionData = [
+    {
+        title: "Skills",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quaerat aperiam ut veritatis voluptas, quae rem ipsam officia, voluptatibus cum commodi quibusdam accusantium nemo. Tempora ea id sunt quasi ut"
+    },
+    {
+        title:"Qualifications",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quaerat aperiam ut veritatis voluptas, quae rem ipsam officia, voluptatibus cum commodi quibusdam accusantium nemo. Tempora ea id sunt quasi ut?"
+    },
+    {
+        title: "Projects",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quaerat aperiam ut veritatis voluptas, quae rem ipsam officia, voluptatibus cum commodi quibusdam accusantium nemo. Tempora ea id sunt quasi ut?"
+    }
+]
+
+export default sectionData;
